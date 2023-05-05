34b0ca526cabe3c2cea9f3d41d9af20bdfc43968
thomaswang0822 <thomaswang0822@outlook.com>
2023-05-05 10:29:49 -0700

    update_target() according to current pos; minor fix

--- Assets/Scripts/AirRacing.cs
+++ Assets/Scripts/AirRacing.cs
@@ -6,23 +6,37 @@ using UnityEngine;
 public class AirRacing : MonoBehaviour
 {
     // Public:
+    public Camera camera;
     public GameObject campus;
     public List<Vector3> checkPts;
+    // should have been private, but we make it public to better switch viewpoint in pause mode
+    public List<GameObject> checkPt_Objs;
     // Private:
     private float sphR_foot = 30.0f;
-    public List<GameObject> checkPt_Objs;
+    private float sphR;
+    // previous and next checkPt sphere indicators: draw line and respawn
+    private GameObject prevSph;
+    private GameObject nextSph;
+    private int nextIdx;
     // Consts:
     private const float inch2meter = 0.0254f;  // inch to meter
     private const float foot2meter = 0.3048f;
     private const string checkPt_fPath = "Assets/Scripts/Sample-track.txt";
+    private readonly Color unfinishedColor = new Color(1.0f, 0.0f, 0.0f, 0.5f);  // half-transparent red
+    private readonly Color targetColor = new Color(0.0f, 0.0f, 1.0f, 1.0f);  // solid blue
+    private readonly Color finishedColor = new Color(0.0f, 0.0f, 1.0f, 0.1f);  // very transparent blue
+
     
     // Start is called before the first frame update
     void Start()
     {
-        // init checking points
-        checkPts = ParseFile();
-        // Instantiate Spheres indicating check points
+        // init checking points and Instantiate Spheres indicating check points
+        checkPts = parse_file(); 
         draw_checkPt();
+        prevSph = checkPt_Objs[0];
+        nextSph = checkPt_Objs[1];
+        nextIdx = 1;
+
         
     }
 
@@ -33,7 +47,7 @@ public class AirRacing : MonoBehaviour
     }
 
     // ********** Helper Functions **********
-    List<Vector3> ParseFile(string filePath=checkPt_fPath)
+    List<Vector3> parse_file(string filePath=checkPt_fPath)
     {
         List<Vector3> positions = new List<Vector3>();
         using (StreamReader reader = new StreamReader(filePath))
@@ -53,12 +67,19 @@ public class AirRacing : MonoBehaviour
         return positions;
     }
 
+
     void draw_checkPt() {
         GameObject spherePrefab = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         // set the color and transparency: half-transparent red
-        spherePrefab.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f, 0.5f);
+        spherePrefab.GetComponent<Renderer>().material.color = unfinishedColor;
         // set the radius to 30.0 feet in meter
-        spherePrefab.transform.localScale = Vector3.one * sphR_foot * foot2meter; 
+        sphR = sphR_foot * foot2meter;
+        spherePrefab.transform.localScale = Vector3.one * sphR; 
+
+        // Also draw a dummy sphere at start location
+        GameObject dummy = Instantiate(spherePrefab, camera.transform.position, Quaternion.identity);
+        dummy.GetComponent<Renderer>().material.color = finishedColor;
+        checkPt_Objs.Add(dummy);
 
         foreach (Vector3 pos in checkPts)
         {
@@ -69,4 +90,24 @@ public class AirRacing : MonoBehaviour
         Destroy(spherePrefab); // destroy the sphere prefab since it's no longer needed
     }
 
+    /// <summary>
+    /// 1. detect if user position is within the radius of nextSph
+    /// 2. if yes: update prevSph and nextSph; update color
+    /// </summary>
+    void update_target() {
+        // current Pos
+        Vector3 currPos = camera.transform.position;
+        
+        if (Vector3.Distance(currPos, nextSph.transform.position) <= sphR)
+        {
+            // update reference
+            nextIdx++;
+            prevSph = nextSph;
+            nextSph = checkPt_Objs[nextIdx];
+
+            // update color
+            prevSph.GetComponent<Renderer>().material.color = finishedColor;
+            nextSph.GetComponent<Renderer>().material.color = targetColor;
+        }
+    }
 }
